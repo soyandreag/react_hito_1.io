f2e3e8ff0093c0235a200643421483b9ace6964b
Andrea Allende <allendeandreas@gmail.com>
2024-12-24 11:42:21 -0300

    ubicación carpeta Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Navbar'
-import Home from './components/Home'
+import Home from './components/Home/Home'
 import Footer from './components/Footer'
 import './App.css'
 
